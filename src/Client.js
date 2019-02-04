f48d8ea750f966d533e8724f98853ec1ee318e9b
iRED <foodrickme@gmail.com>
2019-02-03 17:43:24 -0800

    Unify Errors into one file

--- src/Client.js
+++ src/Client.js
@@ -15,8 +15,7 @@ const Bot = require('./structures/Bot.js');
 const User = require('./structures/User.js');
 const Upvote = require('./structures/Upvote.js');
 const Stats = require('./structures/Stats.js');
-const Ratelimit = require('./structures/Errors/Ratelimit.js');
-const FetchError = require('./structures/Errors/FetchError.js');
+const { Ratelimit, FetchError } = require('./structures/errors.js');
 const { ClientOpts, FetchOpts, PostOpts, MultiFetchOpts } = require('./structures/options.js');
 
 /**
--- src/structures/errors.js
+++ src/structures/errors.js
@@ -39,4 +39,25 @@ class Ratelimit extends Error {
 	}
 }
 
-module.exports = Ratelimit;
+/**
+ * FetchError
+ * @extends {Error}
+ */
+class FetchError extends Error {
+	/**
+     * @param {Response} i
+	 * @param {string} message
+     */
+	constructor(i, message) {
+		super(`${i.status} ${message}`);
+		Object.defineProperty(this, 'name', { value: 'FetchError' });
+		if (Error.captureStackTrace) Error.captureStackTrace(this, FetchError);
+	}
+
+	toString() {
+		return this.message;
+	}
+}
+
+exports.FetchError = FetchError;
+exports.Ratelimit = Ratelimit;
